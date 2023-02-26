ccc88139253115f6edc6dd89bed9532944e465d5
Sebastian Patitucci <patitucciivo@gmail.com>
2023-02-26 16:32:24 -0300

    Modificacion realizada

---
+++ curriculum.js
@@ -0,0 +1,7 @@
+document.getElementById('texto').addEventListener('click', function(){
+    document.getElementById('aboutme').style.visibility = visible;
+})
+
+function funcion(){
+    document.getElementById('aboutme').style.visibility = visible;
+}
